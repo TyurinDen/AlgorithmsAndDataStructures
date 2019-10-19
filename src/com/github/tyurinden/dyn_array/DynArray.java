bb829348b0910812e87d43142158e065f5613e58
Dentttt <tyurinden@ya.ru>
2019-10-20 02:12:03 +0400

    - реализованы некоторые основные методы класса DynArray. - написала часть тестов

--- src/com/github/tyurinden/dyn_array/DynArray.java
+++ src/com/github/tyurinden/dyn_array/DynArray.java
@@ -1,5 +1,8 @@
 package com.github.tyurinden.dyn_array;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class DynArray<T> {
     public T[] array;
     public int count;
@@ -8,28 +11,60 @@ public class DynArray<T> {
 
     public DynArray(Class clz) {
         clazz = clz; // нужен для безопасного приведения типов
-        // new DynArray<Integer>(Integer.class);
-
         count = 0;
-        makeArray(16);
+        capacity = 16;
+        array = (T[]) Array.newInstance(this.clazz, 16);
+    }
+
+    public DynArray(Class clz, int initialCapacity) {
+        clazz = clz;
+        count = 0;
+        capacity = initialCapacity;
+        array = (T[]) Array.newInstance(this.clazz, capacity);
     }
 
     public void makeArray(int new_capacity) {
-        // array = (T[]) Array.newInstance(this.clazz, new_capacity);
-        // ваш код
+        this.capacity = new_capacity;
+        array = Arrays.copyOf(array, new_capacity);
     }
 
     public T getItem(int index) {
-        // ваш код
-        return null;
+        if (index < 0 || index > this.count - 1) {
+            throw new IllegalArgumentException("Index is out of bounds!");
+        }
+        return array[index];
     }
 
     public void append(T itm) {
-        // ваш код
+        if (capacity < count + 1) {
+            makeArray(capacity * 2);
+        }
+        array[count++] = itm;
+    }
+
+    public void appendMany(T... itm) {
+        if (capacity < itm.length + count) {
+            int newCapacity = ((itm.length + count + 1) / capacity + 1) * capacity;
+            makeArray(newCapacity);
+        }
+        int indexTo = itm.length + count;
+        for (int j = 0; count < indexTo; j++) {
+            array[count++] = itm[j];
+        }
     }
 
     public void insert(T itm, int index) {
-        // ваш код
+        if (index < 0 || index > this.count - 1) {
+            throw new IllegalArgumentException("Index is out of bounds!");
+        }
+        if (capacity < count + 1) {
+            makeArray(capacity * 2);
+        }
+        if (count - index >= 0) {
+            System.arraycopy(array, index, array, index + 1, count - index);
+        }
+        array[index] = itm;
+        count++;
     }
 
     public void remove(int index) {
--- src/com/github/tyurinden/dyn_array/DynArrayTest.java
+++ src/com/github/tyurinden/dyn_array/DynArrayTest.java
@@ -8,6 +8,29 @@ public class DynArrayTest {
 
     @Test
     public void makeArray() {
+        DynArray<Integer> integerDynArray = new DynArray<>(Integer.class);
+        assertEquals(16, integerDynArray.capacity);
+        assertEquals(0, integerDynArray.count);
+        integerDynArray.appendMany(1, 2, 3);
+        assertEquals(3, integerDynArray.count);
+        assertEquals(16, integerDynArray.capacity);
+        assertEquals(new Integer(1), integerDynArray.getItem(0));
+        assertEquals(new Integer(2), integerDynArray.getItem(1));
+        assertEquals(new Integer(3), integerDynArray.getItem(2));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void makeArrayWhenCapacityOfArrayIncreases() {
+        DynArray<Integer> integerDynArray = new DynArray<>(Integer.class);
+        integerDynArray.appendMany(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+        assertEquals(16, integerDynArray.count);
+        integerDynArray.append(16);
+        assertEquals(17, integerDynArray.count);
+        assertEquals(32, integerDynArray.capacity);
+        assertEquals(new Integer(0), integerDynArray.getItem(0));
+        assertEquals(new Integer(15), integerDynArray.getItem(15));
+        assertEquals(new Integer(16), integerDynArray.getItem(16));
+        integerDynArray.getItem(integerDynArray.count); // throws IllegalArgumentException
     }
 
     @Test
@@ -18,8 +41,45 @@ public class DynArrayTest {
     public void append() {
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void insert() {
+        DynArray<Integer> integerDynArray = new DynArray<>(Integer.class);
+        integerDynArray.appendMany(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+        assertEquals(16, integerDynArray.capacity);
+        integerDynArray.insert(44, 4);
+        assertEquals(11, integerDynArray.count);
+        assertEquals(new Integer(44), integerDynArray.getItem(4));
+        assertEquals(new Integer(4), integerDynArray.getItem(5));
+        assertEquals(new Integer(5), integerDynArray.getItem(6));
+        assertEquals(new Integer(6), integerDynArray.getItem(7));
+        assertEquals(new Integer(7), integerDynArray.getItem(8));
+        assertEquals(new Integer(8), integerDynArray.getItem(9));
+        assertEquals(new Integer(9), integerDynArray.getItem(10));
+        integerDynArray.getItem(11); // throws IllegalArgumentException
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void insertWhenCapacityOfArrayIncreases() {
+        DynArray<Integer> integerDynArray = new DynArray<>(Integer.class, 5);
+        integerDynArray.appendMany(0, 1, 2, 3, 4);
+        assertEquals(5, integerDynArray.count);
+        assertEquals(5, integerDynArray.capacity);
+        integerDynArray.insert(22, 2);
+        assertEquals(6, integerDynArray.count);
+        assertEquals(10, integerDynArray.capacity);
+        assertEquals(new Integer(22), integerDynArray.getItem(2));
+        assertEquals(new Integer(2), integerDynArray.getItem(3));
+        assertEquals(new Integer(3), integerDynArray.getItem(4));
+        assertEquals(new Integer(4), integerDynArray.getItem(5));
+        integerDynArray.getItem(6); // throws IllegalArgumentException
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void insertWhenArrayIsEmpty() {
+        DynArray<Integer> integerDynArray = new DynArray<>(Integer.class);
+        assertEquals(0, integerDynArray.count);
+        assertEquals(16, integerDynArray.capacity);
+        integerDynArray.insert(22, 0); // throws IllegalArgumentException
     }
 
     @Test
